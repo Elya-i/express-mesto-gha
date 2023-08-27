acc80dd6c4c98398cee11f2220a9a5aefdf2bf02
ElyA_I <elyaizmaylova@gmail.com>
2023-08-27 23:00:49 +0200

    Fix: github autotest error

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ const { validateLoginUser, validateCreateUser } = require('./utils/validation/re
 
 const app = express();
 
-const { PORT, DB_URL } = process.env;
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://localhost:27017/mestodb',
+} = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
